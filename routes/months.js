233e82d0fe9d6c9f75e9e4cd6fd269b09e57d726
Matthew Rowe <mrowe.w@gmail.com>
2017-04-29 19:19:45 -0400

    added check to make sure year query was correct length

--- routes/months.js
+++ routes/months.js
@@ -5,9 +5,14 @@ var router = express.Router()
 router.get('/', function(req, res, next) {
   var year = req.query.year;
   var timestamp = req.query.timestamp;
+  const yearLength = 4
 
-  if(year){
-    if(year.match(/^[12][0-9]{3}/i) || timestamp.match(/1234/i)) {
+  if (year) {
+    if (year.length !=  yearLength) {
+      res.send(404)
+    }
+
+    else if (year.match(/^[12][0-9]{3}/i) || timestamp.match(/1234/i)) {
       res.json({
         year: year,
         timestamp: timestamp
