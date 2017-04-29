e25b4dba6537dd9543df49e1e65c60c67f6aafa7
Matthew Rowe <mrowe.w@gmail.com>
2017-04-29 14:56:40 -0400

    changed index page with view engine

--- routes/months.js
+++ routes/months.js
@@ -4,10 +4,8 @@ var router = express.Router()
 // htpp://localhost:3000/december?year=2015
 router.get('/', function(req, res, next) {
   var year = req.query.year;
-  var timestamp = req.query.timestamp;
 
   if(year.match(/^[12][0-9]{3}/i)){
-    var date = new Date(year);
     res.json({
       year: year
     })
