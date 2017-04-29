713d1540c3a1b6dc9eee5db76cafc973ccb51fa2
Matthew Rowe <mrowe.w@gmail.com>
2017-04-29 17:36:15 -0400

    started implementing error handling for string query

--- routes/months.js
+++ routes/months.js
@@ -5,15 +5,18 @@ var router = express.Router()
 router.get('/', function(req, res, next) {
   var year = req.query.year;
 
-  if(year.match(/^[12][0-9]{3}/i)){
-    res.json({
-      year: year
-    })
+  if(year){
+    if(year.match(/^[12][0-9]{3}/i)) {
+      res.json({
+        year: year
+      })
+    }
   }
 
   else {
-    res.render(404);
+    res.send('add a query to url ?year=2015')
   }
+
 })
 
 module.exports = router
