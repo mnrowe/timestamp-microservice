9c666697c41edc582b808ca0388bd9c4462ab04b
Matthew Rowe <mrowe.w@gmail.com>
2017-04-29 19:38:07 -0400

    added more options for queries in months routes

--- routes/months.js
+++ routes/months.js
@@ -3,18 +3,49 @@ var router = express.Router()
 
 // htpp://localhost:3000/december?year=2015
 router.get('/', function(req, res, next) {
-  var year = req.query.year;
-  var timestamp = req.query.timestamp;
+  var year = req.query.year
+  var timestamp = req.query.timestamp
   const yearLength = 4
+  const timestampLength = 10
 
-  if (year) {
+
+  if (year && timestamp) {
+    if (year.length != yearLength && timestamp.length != timestampLength) {
+      res.send(404)
+    }
+
+    else if(year.match(/^[12][0-9]{3}/i)) {
+      res.json({
+        year: year,
+        timestamp: timestamp
+      })
+    }
+
+    else {
+      res.send(404)
+    }
+  }
+
+  else if (year) {
+
     if (year.length !=  yearLength) {
       res.send(404)
     }
 
-    else if (year.match(/^[12][0-9]{3}/i) || timestamp.match(/1234/i)) {
+    else if (year.match(/^[12][0-9]{3}/i)) {
       res.json({
-        year: year,
+        year: year
+      })
+    }
+
+    else {
+      res.send(404)
+    }
+  }
+
+  else if(timestamp) {
+    if (timestamp.match(/1234/i)) {
+      res.json({
         timestamp: timestamp
       })
     }
@@ -23,7 +54,6 @@ router.get('/', function(req, res, next) {
   else {
     res.send('add a query to url ?year=2015')
   }
-
 })
 
 module.exports = router
