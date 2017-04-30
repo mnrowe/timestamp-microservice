afaf4e4739e6f118d8997d513902b4e7ebfbad52
Matthew Rowe <mrowe.w@gmail.com>
2017-04-29 21:14:24 -0400

    formatted code with prettier

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'API: Timestamp Microservice' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "API: Timestamp Microservice" });
 });
 
 module.exports = router;
