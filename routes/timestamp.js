8157ae257002c6882061faef144f6a4e9b3f658b
Matthew Rowe <mrowe.w@gmail.com>
2017-05-02 16:04:04 -0400

    added route for  just plain unix timestamp

--- routes/timestamp.js
+++ routes/timestamp.js
@@ -2,7 +2,41 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res, next) {
-  console.log(req.baseUrl);
+  const timestamp = parseInt(req.baseUrl.match(/[0-9]{10}/i));
+  const timestampLength = timestamp.length;
+  let day = req.query.day;
+  if (day === undefined) {
+    day = new Date().getDate();
+  }
+
+  if (timestamp.length != timestampLength) {
+    rese.send(404);
+  } else {
+    let timestampConverted = t => {
+      let date = new Date(t * 1000);
+      const months = [
+        "January",
+        "Febuary",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ];
+      const month = months[date.getMonth()];
+      const year = date.getFullYear();
+      return `${month} ${day} ${year}`;
+    };
+    res.json({
+      unix: timestamp,
+      natrual: timestampConverted(timestamp)
+    });
+  }
 });
 
 module.exports = router;
